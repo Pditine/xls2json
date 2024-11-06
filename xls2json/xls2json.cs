04970fb8e46a93b3c8bb8304b8c89831a4d6d403
Pditine <hanfe666@outlook.com>
2024-11-07 02:57:32 +0800

    [feat] 读取excel

--- xls2json/xls2json.cs
+++ xls2json/xls2json.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Data;
 using System.Diagnostics;
 using System.Xml;
+using Excel;
 
 namespace xls2json
 {
     public partial class xls2json : Form
     {
-        private const string ExcelPath = "Excel";
+        private List<string> _excelNames;
+        private string _excelPath = "./Excel/";
+        private string ExcelPath => _excelPath;
+        private string FullExcelPath => Path.GetFullPath(ExcelPath);
         private Log _log;
         public Log Log
         {
@@ -30,8 +35,16 @@ namespace xls2json
 
         private void Convert_Click(object sender, EventArgs e)
         {
-            Log.Info("转换成功");
-            LogBoxToBottom();
+            try
+            {
+                ConvertExcel();
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception.Message + exception.StackTrace);
+                throw;
+            }
+
         }
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -49,14 +62,21 @@ namespace xls2json
 
         private void Init()
         {
-            var files = PreLoadExcelFiles();
-            AddExcelFileItem(files);
+            _excelNames = PreLoadExcelFiles();
+            AddExcelFileItem(_excelNames);
         }
 
         private List<string> PreLoadExcelFiles()
         {
-            if (!Directory.Exists(ExcelPath))
+            if (!Path.Exists(ExcelPath))
+            {
                 Directory.CreateDirectory(ExcelPath);
+                Log.Info($"Excel文件夹不存在，已自动创建 {FullExcelPath}");
+            }else
+            {
+                Log.Info($"从 {FullExcelPath} 加载文件");
+            }
+            
             var files = Directory.GetFiles(ExcelPath);
             var fileList = new List<string>(files);
             fileList.RemoveAll(f => !f.EndsWith(".xls") && !f.EndsWith(".xlsx"));
@@ -83,19 +103,57 @@ namespace xls2json
             LogBox.ScrollToCaret();
         }
 
-        private void LoadExcel()
+        private void LoadExcel(string excelPath)
         {
+            if (!File.Exists(excelPath))
+            {
+                Log.Error($"文件不存在 {excelPath}");
+                return;
+            }
+            using var fs = File.Open(excelPath, FileMode.Open, FileAccess.Read);
+            var excelDataReader = ExcelReaderFactory.CreateBinaryReader(fs);
+            //todo: 分支判断
+            //var excelDataReader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+            var result = excelDataReader.AsDataSet();
+            if(result == null)
+            {
+                Log.Error($"文件读取失败 {excelPath}");
+                return;
+            }
             
+            foreach (DataTable table in result.Tables)
+            {
+                LoadSheet(table);
+            }
+            
+            fs.Close();
         }
         
-        private void LoadSheet()
+        private void LoadSheet(DataTable table)
         {
-               
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (DataColumn column in table.Columns)
+                {
+                    var value = row[column];
+                    Log.Debug(value.ToString());
+                }
+            }
         }
 
         private void WriteInJson()
         {
             
+        }
+
+        private void ConvertExcel()
+        {
+            foreach (string excelName in _excelNames)
+            {
+                LoadExcel(excelName);
+            }
+
+            LogBoxToBottom();
         }
     }
 }
